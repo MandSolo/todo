ace711076e02a58979a3b769434e588b45c4b430
Mand Cashin <mandcashin@hotmail.co.uk>
2021-07-01 23:49:15 +0100

    feat: conditional logic to show message if there are no todo items

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,20 +2,23 @@ import React from "react";
 import PropTypes from "prop-types";
 import Todo from "./Todo";
 
-const TodoList = ({ todos, toggleTodo, deleteTodo }) => (
-  <ul>
-    {todos.map((todo) => {
-      return (
-        <Todo
-          key={todo.id}
-          {...todo}
-          handleToggle={() => toggleTodo(todo.id)}
-          handleDelete={() => deleteTodo(todo.id)}
-        />
-      );
-    })}
-  </ul>
-);
+const TodoList = ({ todos, toggleTodo, deleteTodo }) =>
+  todos.length ? (
+    <ul>
+      {todos.map((todo) => {
+        return (
+          <Todo
+            key={todo.id}
+            {...todo}
+            handleToggle={() => toggleTodo(todo.id)}
+            handleDelete={() => deleteTodo(todo.id)}
+          />
+        );
+      })}
+    </ul>
+  ) : (
+    <div>You have nothing todo!</div>
+  );
 
 TodoList.propTypes = {
   todos: PropTypes.arrayOf(
